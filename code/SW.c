784afff43a6c4c86e7aeb911f33770255e6885ea
Niranjan Rajkumar <128575600+Niranjanr0682@users.noreply.github.com>
2023-08-25 08:54:56 +0530

    .

--- code/SW.c
+++ code/SW.c
@@ -3,26 +3,25 @@
 #include<stdio.h>
 void main()
 {
-char sender[50],receiver[50];
-int i,winsize;
+char s[50],r[50];
+int i,ws;
 
- printf("\n ENTER THE WINDOWS SIZE : ");
-scanf("%d",&winsize);
- printf("\n SENDER WINDOW IS EXPANDED TO STORE MESSAGE OR WINDOW \n");
- printf("\n ENTER THE DATA TO BE SENT: ");
+ printf("WINDOWS SIZE : ");
+scanf("%d",&ws);
+ printf("\n SENDER WINDOW \n");
+ printf("\n THE DATA TO SENT: ");
 fflush(stdin);
-gets(sender);
-for(i=0;i<winsize;i++)
-receiver[i]=sender[i];
-receiver[i]=NULL;
- printf("\n MESSAGE SEND BY THE SENDER:\n");
- puts(sender);
- printf("\n WINDOW SIZE OF RECEIVER IS EXPANDED\n");
- printf("\n ACKNOWLEDGEMENT FROM RECEIVER \n");
-for(i=0;i<winsize;i++);
+gets(s);
+for(i=0;i<ws;i++)
+r[i]=s[i];
+r[i]=NULL;
+ printf("\n MESSAGE BY SENDER:\n");
+ puts(s);
+ printf("\n WINDOW OF RECEIVER IS EXPANDED\n");
+ printf("\n ACK FROM RECEIVER \n");
+for(i=0;i<ws;i++);
 printf("\n ACK:%d",i);
  printf("\n MESSAGE RECEIVED BY RECEIVER IS : ");
- puts(receiver);
- printf("\n WINDOW SIZE OF RECEIVER IS SHRINKED \n");
+ puts(r);
 
 }
